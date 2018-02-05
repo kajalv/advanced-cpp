dc31f275998d64144bb7180201e56722f3687417
Kajal Varma <Kajal_Varma@intuit.com>
2018-02-05 21:09:26 +0530

    Ring buffer

--- cPlusPlus/headers/NestedTemplates.h
+++ cPlusPlus/headers/NestedTemplates.h
@@ -13,6 +13,42 @@ class ring
 {
 public:
 	class iterator; // implementing it here itself is generally not good practice, as it can get quite complicated and messy while nesting. So we can take it outside the class.
+
+	// ring buffer impl
+	ring(int size) : m_size(size), m_values(NULL), m_pos(0) // initialize in the order they are declared to prevent warnings
+	{
+		m_values = new T[size];
+	}
+
+	~ring()
+	{
+		delete[] m_values;
+		m_values = NULL;
+	}
+
+	int size()
+	{
+		return m_size;
+	}
+
+	void add(T value)
+	{
+		m_values[m_pos++] = value;
+		if (m_pos == m_size)
+		{
+			m_pos = 0;
+		}
+	}
+
+	T& get(int pos) // usually STL containers return references
+	{
+		return m_values[pos];
+	}
+
+private:
+	T *m_values;
+	int m_size;
+	int m_pos;
 };
 
 // iterator has to be aware of the type while dereferencing, so has to use the same typename as the ring
--- cPlusPlus/src/cpp11/NestedTemplateClass.cpp
+++ cPlusPlus/src/cpp11/NestedTemplateClass.cpp
@@ -6,10 +6,15 @@ void runNestedTemplate()
 	ring<string>::iterator it;
 	it.print();
 
-	/*ring<string> textring(3);
+	ring<string> textring(3);
 
 	textring.add("one");
 	textring.add("two");
 	textring.add("three");
-	textring.add("four"); // overwrites 'one'*/
+	textring.add("four"); // overwrites 'one'
+
+	for (int i = 0; i < textring.size(); i++)
+	{
+		cout << textring.get(i) << endl;
+	}
 }
