de808edae995d246edc358c40864c399af4515cb
Kajal Varma <Kajal_Varma@intuit.com>
2018-02-08 18:31:29 +0530

    Capturing 'this' with lambdas

--- cPlusPlus/src/cpp11/Lambdas.cpp
+++ cPlusPlus/src/cpp11/Lambdas.cpp
@@ -16,6 +16,49 @@ void runDivide(double(*divide)(double, double))
 	cout << rval << endl;
 }
 
+class TestCaptureThis
+{
+public:
+	void run()
+	{
+		int three{ 3 };
+		int four{ 4 };
+
+		auto pL1 = [three, four]() // cannot capture instance variables until you pass 'this'! So here one and two cannot be accessed
+		{
+			cout << three << endl;
+			cout << four << endl;
+		};
+		pL1();
+
+		auto pL2 = [this, three, four]() // capture this by reference (always, cannot be done by value. So instance variables can be changed.)
+		{
+			one = 111;
+			cout << one << endl;
+			cout << two << endl;
+			cout << three << endl;
+			cout << four << endl;
+		};
+		pL2(); // no need to pass 'this' explicitly, it's implicit
+
+		auto pL3 = [three, this]() // this can also be passed in any order: need not be the first in the list
+		{
+			two = 222;
+			cout << one << endl;
+			cout << two << endl;
+			cout << three << endl;
+		};
+		pL3();
+
+		cout << one << endl; // instance variable changed because 'this' is passed by reference
+		cout << two << endl;
+	}
+
+private:
+	int one{ 1 };
+	int two{ 2 };
+};
+
 // runners
 void runBasic()
 {
@@ -80,9 +123,16 @@ void runCapture()
 	cout << one << endl;
 }
 
+void runCaptureThis()
+{
+	TestCaptureThis tester;
+	tester.run();
+}
+
 void runLambdas()
 {
 	runBasic();
 	runParamsAndReturn();
 	runCapture();
+	runCaptureThis();
 }
